d16d6ebf227fba02f278f1ac1f2b217cb4335789
WanomiR <wanomir@yandex.ru>
2023-10-30 17:10:55 +0300

    blogsite - 2

--- blogsite/mainapp/views.py
+++ blogsite/mainapp/views.py
@@ -1,19 +1,13 @@
 from django.shortcuts import render, redirect
+from .models import Article, Comment
+from django.http import HttpResponseNotFound
 
 # Create your views here.
 
-posts = [
-    {
-        "Title": "fjfjjf",
-        "Description": "gjgj",
-        "Author": "gjjg",
-        "Date": "gjgjjg"
-     }
-]
-
 
 def indexpage(request):
-    return render(request, "index.html", {"articles": posts, "page": "index"})
+    articles = Article.objects.all()
+    return render(request, "index.html", {"articles": articles, "page": "index"})
 
 
 def aboutpage(request):
@@ -29,3 +23,22 @@ def contactpage(request):
         with open("/Users/vladbax6/Codding/code_works/Sites/Django/CourseBlog/Backend/blogsite/mainapp/contact_resuls.txt", "a") as file:
             file.writelines(f"Name: {request.POST['name']}, Email: {request.POST['email']}, Subject: {request.POST['subject']}")
         return redirect(contactpage)
+
+
+def articlepage(request, article_id):
+    article = Article.objects.filter(id=article_id).first()
+    if article:
+        comments = Comment.objects.filter(Article=article).all()
+        return render(request, "article.html", {"article": article, "comments": comments})
+
+    return HttpResponseNotFound("Article not found")
+
+
+def commentpost(request, article_id):
+    if request.method == "POST":
+        article = Article.objects.filter(id=article_id).first()
+        if 'name' in request.POST and 'email' in request.POST and 'message' in request.POST:
+            article.new_comment(request.POST)
+            return redirect(articlepage, article_id)
+
+    return HttpResponseNotFound("404")
